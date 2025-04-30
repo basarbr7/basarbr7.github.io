4b7481c4b233e91955ced007ff167d09ccdbe0e1
M.A. Basar <basarbr7@gmail.com>
2025-04-30 19:46:50 +0600

    added scroll smooth by js

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-document.getElementById('menu-toggle').addEventListener('click', () => {
-    const menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
+  document.getElementById('menu-toggle').addEventListener('click', () => {
+      const menu = document.getElementById('mobile-menu');
+      menu.classList.toggle('hidden');
+    });
+
+
+  const navbar = document.querySelector("#nav")
+  window.addEventListener("scroll", (e)=>{
+    let scrolly = window.scrollY
+    if(scrolly>100){
+      navbar.classList.add("fixed", "top-0", "header-animation")
+      navbar.classList.remove("absolute");
+    }else{
+      navbar.classList.remove("fixed", "top-0", "header-animation")
+      navbar.classList.add("absolute");
+    }
+    
+  })
+
+
+  // Smooth scroll 
+  const anchorsAll = document.querySelectorAll('a[href^="#"]')
+
+  anchorsAll.forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      const navbarHeight = 60;
+      const top = target.getBoundingClientRect().top + window.scrollY - navbarHeight;
+      
+      window.scrollTo({
+        top,
+        behavior: 'smooth'
+      });
+    });
   });
 
 
-const navbar = document.querySelector("#nav")
-window.addEventListener("scroll", (e)=>{
-  let scrolly = window.scrollY
-  if(scrolly>100){
-    navbar.classList.add("fixed", "top-0", "header-animation")
-    navbar.classList.remove("absolute");
-  }else{
-    navbar.classList.remove("fixed", "top-0", "header-animation")
-    navbar.classList.add("absolute");
-  }
   
-})
+
+  
