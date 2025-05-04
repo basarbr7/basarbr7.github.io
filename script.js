57dbab944d07c01b431c0928e26395cb72f33879
M.A. Basar <basarbr7@gmail.com>
2025-05-04 22:53:47 +0600

    added AOS animation fade

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+  
+  AOS.init();
+
   document.getElementById('menu-toggle').addEventListener('click', () => {
       const menu = document.getElementById('mobile-menu');
       menu.classList.toggle('hidden');
